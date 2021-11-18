ee1357ca88b7aa4073225d738a343c0d1ae630e2
JesusArim <92616948+JesusArim@users.noreply.github.com>
2021-11-18 17:36:03 -0600

    Update Parametros.java

--- Parametros.java
+++ Parametros.java
@@ -7,8 +7,8 @@ public class Parametros{
     // Metodo principal donde asignamos valores con parametros por valor y por referencia. 
     public static void main(String[] args){
 	int edad1;
-	int [] edad2 = new int [3];
-	    edad1=10;
+	int [] edad2 = new int [2];
+	   edad1 = 10;
 	edad2[0] = 20;
 	edad2[1] = 30;
         // Imprimimimos los valores asignados. 
@@ -19,15 +19,15 @@ public class Parametros{
 	System.out.println("EDAD3: "+ edad1+"años" + " " + edad2[0]+"años" +" "+ edad2[1]+"años");
      }
     /*
-     * Metodo modificador para modificar un dato por paso de  parametro por valor y otros dos datos por paso de parametro por referencia.
-     * @param EDAD1 50
-     * @param EDAD2[0] 60
-     * @param EDAD2[1] 70
+     * Metodo para modificar un dato por paso de  parametro por valor y otros dos datos por paso de parametro por referencia.
+     * @param EDAD1 20
+     * @param EDAD2[0] 25
+     * @param EDAD2[1] 50
      * */
     public static void set(int EDAD1, int [] EDAD2){
-	EDAD1 = 50;
-	EDAD2[0] = 60;
-	EDAD2[1] = 70;
+	EDAD1 = 20;
+	EDAD2[0] = 25;
+	EDAD2[1] = 50;
 	// Imprimimos EDAD1 por paso de parametro por valor.
 	// Imprimimos EDAD2 por paso de parametro por referencia indicada.
 	System.out.println("EDAD2: "+ EDAD1+"años" + " " + EDAD2[0]+"años" +" "+ EDAD2[1]+"años");
